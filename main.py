bc720a95512a5c72ec282f2e6d042aff60681a87
Yankos <xardziopl@gmail.com>
2023-11-16 16:14:45 +0100

    Działa dodawanie z bazy 2100 elementów Modules - problem: można dodać CPU i słabiutko wtedy

--- main.py
+++ main.py
@@ -1,67 +1,74 @@
-import xml.etree.ElementTree as ET
+import lxml.etree as etree
+import string
+import random
+import os
+from os import listdir
+from os.path import isfile, join, isdir
+
+which_disk_br = 'C:/'
 
 # inputs
 as_project_path = r'C:\projects'
 filename = r'Clear'
+#chosen_module
 
+# KOD TO SZUKANIA PLIKÓW MODUŁU
+def find_modules(disk_path):
+    for dirpath, dirnames, filenames in os.walk(disk_path):
+        for dirname in dirnames:
+            if dirname == "BRAutomation":
+                dirname = os.path.join(dirpath, dirname)
+                mypath = dirpath + r'BRAutomation/AS412/AS/Hardware/Modules'
+                modules = [f for f in listdir(mypath) if isdir(join(mypath, f))]
+                return modules,mypath
+
+
+def find_module_version(module,path):
+    path = path + '/' + module
+    version = [f for f in listdir(path) if isdir(join(path, f))]
+    return version
 
 def get_to_file(path, name):
     input_path = path + chr(92) + name + r'\Physical\Config1'
-    pathHWL = input_path + r'\Hardware.hwl'
-    pathHW = input_path + r'\Hardware.hw'
-    rootHWL=3
-    # try:
-    #     treeHWL = ET.parse(pathHWL)
-    #     rootHWL = treeHWL.getroot()
-    # except:
-    #     print('Cannot get to file in destination:', pathHWL)
-    #     exit()
+    pathHWL_fun = input_path + r'\Hardware.hwl'
+    pathHW_fun = input_path + r'\Hardware.hw'
     try:
-        treeHW = ET.parse(pathHW)
-        rootHW = treeHW.getroot()
-
+        treeHWL_fun = etree.parse(pathHWL_fun)
+        rootHWL_fun = treeHWL_fun.getroot()
     except:
-        print('Cannot get to file in destination:', pathHW)
+        print('Cannot get to file in destination:', pathHWL_fun)
         exit()
+    try:
+        treeHW_fun = etree.parse(pathHW_fun)
+        rootHW_fun = treeHW_fun.getroot()
+    except:
+        print('ERROR - Cannot get to file in destination:', pathHW_fun)
+        exit()
+    return rootHW_fun, rootHWL_fun, pathHW_fun, pathHWL_fun, treeHW_fun, treeHWL_fun
 
-    return rootHW, rootHWL
+
+def get_random_string(length):
+    letters = string.ascii_letters
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
 
 
-def show_attrib(root):
-    for x in root:
-        print(x.tag[40:])
-        print(x.attrib)
+def add_element(root_hw, root_hwl, path_hw, path_hwl, type, version):
+    str = get_random_string(1)
+    element = etree.SubElement(root_hwl, 'Module', Name=type + str, Type=type, X='450', Y='450')
+    root_hwl[1][0].append(element)
+    etree.ElementTree(root_hwl).write(path_hwl)
+    print(element.tag, element.attrib)
 
+    element = etree.SubElement(root_hw, 'Module', Name=type + str, Type=type, Version=version)
+    root_hw.append(element)
+    etree.ElementTree(root_hw).write(path_hw)
+    print(element.tag, element.attrib)
 
-# for x in rootHW.iter(rootHW[0].tag):
-#     print(x.tag[40:])
-#     print(x.attrib)
-#
-#
-# print(myroot[1][0].attrib.keys())
-# print(myroot[1][0].attrib.values())
-# print(myroot[1][0].attrib.get('Type')) #szuka w keys
-# print(myroot[1][0].attrib)
-#
-
-# #SPRAWDZANIE ZMIAN - NIEKONIECZNE
-# print('=================OLD=============================')
-# file = open(route,'r')
-# print(file.read())
-
-# myroot[1][3].set('Name','AAAA')
-# rootHW.makeelement('tag', a)
-# ET.SubElement(rootHW[1][0], 'AAAAAAAAAAAAAA', a)
-# #
-#
-# treeHW.write(pathHW)
-# print('=================NEW=============================')
-# file = open(route,'r')
-# print(file.read())
-#
-#
-#
 if __name__ == '__main__':
-    rootHW, rootHWL = get_to_file(as_project_path, filename)
-    show_attrib(rootHW)
-
+    module_list,module_path = find_modules(which_disk_br) #lista modułów zainstalowanych na dysku C
+    chosen_module = module_list[300]
+    module_version = find_module_version(chosen_module,module_path)
+    rootHW, rootHWL, pathHW, pathHWL, treeHW, treeHWL = get_to_file(as_project_path, filename)
+    add_element(rootHW, rootHWL, pathHW, pathHWL, chosen_module, module_version[0])
+    print('END')
