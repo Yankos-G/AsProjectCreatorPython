52e9c3d4c6b68236472f08153e2371f59fda39f5
Yankos <xardziopl@gmail.com>
2023-12-21 13:38:51 +0100

    adding 3 in 1 and connect to PLC

--- main.py
+++ main.py
@@ -6,14 +6,24 @@ import os
 from os import listdir
 from os.path import isfile, join, isdir
 import sys
-#a=sys.argv[1]
+
+# a=sys.argv[1]
+
+# INPUTS
 which_disk_br = 'C:/'
-LIB_DESCRIPT = 'program added library'
+
 
 as_project_path = r'C:\projects'
 filename = r'TESTLD'
 
 library_path = r'C:\BRAutomation\AS\Library'
+
+PLCname = 'X20CP1584'
+
+# CONSTANT
+IF6busy = False
+last = ''
+LIB_DESCRIPT = 'program added library'
 
 # # KOD TO SZUKANIA PLIKÓW BIBLIOTEK
 # def find_modules(disk_path):
@@ -26,7 +36,6 @@ library_path = r'C:\BRAutomation\AS\Library'
 #                 return modules,mypath
 
 
-
 # KOD TO SZUKANIA PLIKÓW MODUŁU I BIBLIOTEK
 def find_modules(disk_path):
     for dirpath, dirnames, filenames in os.walk(disk_path):
@@ -37,7 +46,7 @@ def find_modules(disk_path):
                 Library_path = dirpath + r'BRAutomation/AS/Library'
                 modules = [f for f in listdir(BRAutomation_path) if isdir(join(BRAutomation_path, f))]
                 libraries = [g for g in listdir(Library_path) if isdir(join(Library_path, g))]
-                return modules,BRAutomation_path,libraries
+                return modules, BRAutomation_path, libraries
 
 
 def find_module_version(module, path):
@@ -78,7 +87,9 @@ def get_random_string(length):
     return result_str
 
 
-def add_element(root_hw, root_hwl, path_hw, path_hwl, type, version,m_list, m_path, tree_hw):
+def add_IO(root_hw, root_hwl, path_hw, path_hwl, type, version, m_list, m_path, tree_hw):
+    global IF6busy
+    global last
     str = get_random_string(1)
     element = etree.SubElement(root_hwl, 'Module', Name=type + str, Type=type, X='450', Y='450')
     root_hwl[1][0].append(element)
@@ -89,36 +100,50 @@ def add_element(root_hw, root_hwl, path_hw, path_hwl, type, version,m_list, m_pa
     root_hw.append(element)
     print(element.tag, element.attrib)
 
-    # <Module Name="X20AI2632-1Q" Type="X20AI2632-1" Version="1.2.0.0">
-    #   <Connection Connector="SS1" TargetModule="X20TB12Q" TargetConnector="SS" />
-    #   <Connection Connector="SL" TargetModule="X20BM11Q" TargetConnector="SL1" />
-    # </Module>
-
-    # JEŻELI TO MODUŁ IO TO DODAJ TO ABY POŁACZYĆ
-    # MODULES
     typeTB = m_list[m_list.index('X20TB12')]
     typeBM = m_list[m_list.index('X20BM11')]
     m_verTB = find_module_version(typeTB, m_path)
     m_verBM = find_module_version(typeBM, m_path)
-    print(typeTB)
-    print(m_verTB)
-    elementTB = etree.SubElement(root_hw, 'Module', Name=typeTB + str, Type=typeTB, Version=m_verTB[0])
+    nameBM = typeBM + str
+    nameTB = typeTB + str
+    print(nameBM)
+    print(nameTB)
+    elementTB = etree.SubElement(root_hw, 'Module', Name=nameTB, Type=typeTB, Version=m_verTB[0])
     root_hw.append(elementTB)
-    elementBM = etree.SubElement(root_hw, 'Module', Name=typeBM + str, Type=typeBM, Version=m_verBM[0])
+    elementBM = etree.SubElement(root_hw, 'Module', Name=nameBM, Type=typeBM, Version=m_verBM[0])
     root_hw.append(elementBM)
 
-    # CONNECTIONS - DODANIE CHILD DO ELEMENTU
-    etree.SubElement(element, 'Connection', Connector='SL', TargetModule=typeBM + str,
-                                       TargetConnector='SL1')
-    etree.SubElement(element, 'Connection', Connector='SS1', TargetModule=typeTB + str,
-                                        TargetConnector='SS')
+    # CONNECTIONS - DODANIE CHILD DO ELEMENTU (MODUŁ GŁOWNY Z POMOCNYMI)
+    etree.SubElement(element, 'Connection', Connector='SL', TargetModule=nameBM,
+                     TargetConnector='SL1')
+    etree.SubElement(element, 'Connection', Connector='SS1', TargetModule=nameTB,
+                     TargetConnector='SS')
+    if not IF6busy:
+        etree.ElementTree(root_hw).write(path_hw)
+        # CONNECTIONS - DODANIE CHILD DO BM (PODSTAWKA DO AKTUALNIE WOLNEGO SLOTA)
+        ns = {'hw': 'http://br-automation.co.at/AS/Hardware'}
+        a = "//hw:Module[@Name='{}']/hw:Connection/@TargetConnector".format(nameBM)
+        checkIF6slot = rootHW.xpath(a, namespaces=ns)
+        print(checkIF6slot)
 
+        print(last)
+        etree.SubElement(elementBM, 'Connection', Connector='X2X1', TargetModule=PLCname,
+                         TargetConnector='IF6')
+        last = nameBM
+        IF6busy = True
+    else:
+        print(last)
+        etree.SubElement(elementBM, 'Connection', Connector='X2X1', TargetModule=last,
+                         TargetConnector='X2X2')
+        last = nameBM
 
     # ZAPIS DO PLIKU HW
     etree.ElementTree(root_hw).write(path_hw)
 
+    return last
 
-def add_library(root, path, name,descript):
+
+def add_library(root, path, name, descript):
     element = etree.SubElement(root, 'Object', Type="Library", Language="Binary", Description=descript)
     element.text = name
     root[0].append(element)
@@ -129,8 +154,7 @@ def add_library(root, path, name,descript):
 
 
 if __name__ == '__main__':
-    module_list, module_path, libraries = find_modules(which_disk_br) #lista modułów zainstalowanych na dysku C
-
+    module_list, module_path, libraries = find_modules(which_disk_br)  # lista modułów zainstalowanych na dysku C
 
     # 0 - 951 STARSZE I ROZNE
     # 952  - 1455 ACOPOSY
@@ -141,12 +165,17 @@ if __name__ == '__main__':
 
     rootHW, rootHWL, rootLib, pathHW, pathHWL, pathLib, treeHW = get_to_files(as_project_path, filename)
 
-    add_element(rootHW, rootHWL, pathHW, pathHWL, chosen_module, module_version[0], module_list, module_path,treeHW)
+    add_IO(rootHW, rootHWL, pathHW, pathHWL, chosen_module, module_version[0], module_list, module_path, treeHW)
+    add_IO(rootHW, rootHWL, pathHW, pathHWL, chosen_module, module_version[0], module_list, module_path, treeHW)
 
+    # print(rootHW.find('Hardware'))
+    # a = treeHW.findall('{http://br-automation.co.at/AS/Hardware}Module')
+    # print(a)
+    # print(type(a))
 
     # for neighbor in rootHW.iter():
     #     print(neighbor.attrib)
 
     # add_library(rootLib, pathLib, libraries[105], LIB_DESCRIPT)
-    print(etree.tostring(rootHW, pretty_print=True,encoding='unicode'))
+    # print(etree.tostring(rootHW, pretty_print=True, encoding='unicode'))
     print('END')
